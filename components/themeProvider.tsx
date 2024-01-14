2ad1d639fb8f5c80c3e33faa42bece09cadb0544
orl0pl <orlyyt@int.pl>
2024-01-14 19:44:42 +0100

    changed handling

--- components/themeProvider.tsx
+++ components/themeProvider.tsx
@@ -1,51 +1,67 @@
 // ThemeContext.js
 import { argbFromHex } from "@material/material-color-utilities";
-import React, { createContext, useContext, useState } from "react";
-import { themeFromSourceColor, Scheme } from "mcu-extra";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { themeFromSourceColor, Scheme, applyTheme } from "mcu-extra";
+import {Theme} from "mcu-extra";
 
 interface IThemeContext {
-	theme: "light" | "dark";
+	mode: "light" | "dark";
 	sourceColor: string;
-	toggleTheme: (theme?: "light" | "dark") => void;
+	toggleMode: (mode?: "light" | "dark") => void;
 	updateSourceColor: (newColor: string) => void;
 	scheme: Scheme;
 }
 
 const defaultValues: IThemeContext = {
-	theme: "light",
+	mode: "light",
 	sourceColor: "#4400ee",
 	scheme: themeFromSourceColor(argbFromHex("#4400ee")).schemes["light"],
-	toggleTheme: function (): void {},
+	toggleMode: function (): void {},
 	updateSourceColor: function (newColor: string): void {},
 };
 const ThemeContext = createContext(defaultValues);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-	const [theme, setTheme] = useState<"light" | "dark">("light");
+	const [mode, setMode] = useState<"light" | "dark">("light");
 	const [sourceColor, setSourceColor] = useState("#4400ee");
 	const [scheme, setscheme] = useState(
 		themeFromSourceColor(argbFromHex("#4400ee"), []).schemes["light"]
 	); // Store the generated theme colors here
+	const [theme, setTheme] = useState<Theme>(
+		themeFromSourceColor(argbFromHex("#4400ee"), [])
+	)
+	
 
-	const toggleTheme = (utheme?: "light" | "dark") => {
-		setTheme(utheme ? utheme : (prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+	const toggleMode = (utheme?: "light" | "dark") => {
+		setMode(utheme ? utheme : (prevTheme) => (prevTheme === "light" ? "dark" : "light"));
 		setscheme(
-			themeFromSourceColor(argbFromHex(sourceColor), []).schemes[
-				utheme ? utheme : theme === "light" ? "dark" : "light"
+			theme.schemes[
+				utheme ? utheme : mode === "light" ? "dark" : "light"
 			]
 		);
 	};
 
 	const updateSourceColor = (newColor: string) => {
 		setSourceColor(newColor);
-		setscheme(themeFromSourceColor(argbFromHex(newColor), []).schemes[theme]);
+		const generatedTheme = themeFromSourceColor(argbFromHex(newColor), []);
+		setscheme(generatedTheme.schemes[mode]);
 	};
 
+	useEffect(()=>{
+		if(document !== undefined) {
+			if (document.body !== undefined) {
+				applyTheme(theme, {target: document.body, dark: mode === "dark"});
+			}
+		}
+	}, [])
+
 	return (
-		<ThemeContext.Provider value={{ theme, toggleTheme, sourceColor, updateSourceColor, scheme }}>
+		<ThemeContext.Provider value={{ mode, toggleMode, sourceColor, updateSourceColor, scheme }}>
 			{children}
 		</ThemeContext.Provider>
 	);
+
+
 }
 
 export function useTheme() {
