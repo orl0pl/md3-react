d43925e93e3085dc19bd842d48bef0ca5a9a5b9a
orl0pl <orlyyt@int.pl>
2024-01-21 14:25:43 +0100

    rollup setup

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import { ThemeProvider, Text } from 'md3-react';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div>
+      <Text>
+        A
+      </Text>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
